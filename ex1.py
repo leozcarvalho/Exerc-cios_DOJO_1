e53a10814d1d21ecce87c64f9274b2644bc9e3a3
leozcarvalho <64977020+leozcarvalho@users.noreply.github.com>
2021-07-01 10:05:23 -0300

    Update ex1.py

--- ex1.py
+++ ex1.py
@@ -1,19 +1,31 @@
-def perguntar(pergunta):
-    resposta = ''
-    respostasValidas = ['s' ,'sim','Sim','SIM' ,'n' ,'nao' ,'Não','não','NÃO','NAO']
-    while True:
-        resposta = input(pergunta)
-        if resposta in respostasValidas:
-            return resposta
-        else:
-            print('Reposta Invalida!')
+respostasPositivas = ['s' ,'sim','Sim','SIM' ]
     
+respostaNegativas = ['n' ,'nao' ,'Não','não','NÃO','NAO']
 
+def perguntar(pergunta):
+
+    resposta = ''
+
+    while True:
+
+        resposta = input(pergunta)
+
+        if resposta in respostasPositivas or resposta in respostaNegativas:
+
+            return resposta
+
+        else:
+
+            print('Reposta Invalida!')
 
 respostas = []
+
 sim = 0
+
 nao = 0
+
 print('Ocorreu um assasinato e você está sendo interrogado.\nResponda as perguntas com (S)im ou (N)ão ')
+
 respostas.append(perguntar('Telefonou para a vitima? '))
 
 respostas.append (perguntar('Esteve no local do crime? '))
@@ -27,19 +39,27 @@ respostas.append (perguntar('Já trabalhou com a vítima? '))
 print(respostas)
 
 for i in range(5):
-    if(respostas[i] == 's'):
-            sim = sim + 1
-    elif respostas[i] == 'n':
+
+    if respostas[i] in respostasPositivas:
+
+        sim = sim + 1
+
+    elif respostas[i] in respostaNegativas:
+
         nao = nao + 1
-    
 
+if sim == 5:
 
-if sim == 5 and nao == 0:
     print('Assassino')
-elif sim == 4 or sim == 3:
-    print('Cumplice')
-elif sim == 2:
-    print ('Suspeito')
-else:
-    print ('Inocente')
 
+elif sim == 4 or sim == 3:
+
+    print('Cumplice')
+
+elif sim == 2:
+
+    print ('Suspeito')
+
+else:
+
+    print ('Inocente')
