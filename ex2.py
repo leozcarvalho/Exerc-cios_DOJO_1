90e9bd508a71153367850653dec29c2a85da004b
leozcarvalho <64977020+leozcarvalho@users.noreply.github.com>
2021-07-01 10:07:04 -0300

    Update ex2.py

--- ex2.py
+++ ex2.py
@@ -1,61 +1,82 @@
-valor = []
-x = int(0)
-aux = []
-aux1 = []
-aux2 = []
-soma = 0
-y = True
+valores = [] 
+auxiliar = []
 
-while y == True:
-    x = int(input('Insira um valor positivo para o andamento da aplicação ou digite (-1) para fechar a aplicação:\n'))
-    if x == -1:
-        y = False
-   
-    elif x < -1:
+continuar = True
+
+while continuar == True:
+
+    entrada = int(input('Insira um valor positivo para o andamento da aplicação ou digite (-1) para fechar a aplicação:\n'))
+
+    if entrada == -1:
+
+        continuar = False
+
+    elif entrada < -1:
+
         print('ERRO: Insira um valor positivo!')
 
     else:
-        valor.append(x)  
 
-        
-if len(valor) > 0:   
-    print('\nNumero de valores lidos: {}'.format(len(valor)))
-    print(valor)
+        valores.append(entrada)  
 
-    i = len(valor)
+if len(valores) > 0:   
+
+    print('\nNumero de valores lidos: {}'.format(len(valores)))
+
+    print(valores)
 
     print('Ordem inversa da lista')
 
-    for passo in valor:
+    i = len(valores)
+
+    for passo in valores:
+
         i = i -1
-        aux.append(valor[i])
-    print(aux)
+
+        auxiliar.append(valores[i])
+
+    print(auxiliar)
 
     print('Soma dos valores da lista')
 
-    for passo in range(0, len(valor)):
-        soma = soma + valor[passo]
+    soma = 0
+
+    for passo in range(0, len(valores)):
+
+        soma = soma + valores[passo]
+
     print(soma)
 
     print('Média dos valores da lista')
 
-    media = soma / len(valor)
+    media = soma / len(valores)
+
     print(media)
 
     print('Valores acima da média')
 
-    for passo in range(0, len(valor)):
-        if valor[passo] > media:
-            aux2.append(valor[passo])
-    print(aux2)
+    auxiliar.clear()
 
+    for passo in range(0, len(valores)):
+
+        if valores[passo] > media:
+
+            auxiliar.append(valores[passo])
+
+    print(auxiliar)
 
     print('Valores da lista que são abaixo de 7')
 
-    for passo in range(0, len(valor)):
-        if valor[passo] < 7:
-            aux1.append(valor[passo])
-    print(aux1)
-        
+    auxiliar.clear()
+
+    for passo in range(0, len(valores)):
+
+        if valores[passo] < 7:
+
+            auxiliar.append(valores[passo])
+
+    print(auxiliar)
+
     print('\nO programa será finalizado')
+
     input('')
